1b7b4aab5d3a58d24a6b62dee3c0ed8f94ab5397
josungwong <110151451+josungwong@users.noreply.github.com>
2025-05-27 14:59:14 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import FinanceDataReader as fdr
+import streamlit as st
+import datetime
+
+with st.sidebar:
+    date = st.date_input("조회 시작을 선택해주세요.",
+                        datetime.datetime(2024,1,1))
+    code = st.text_input("종목코드",value='',placeholder="종목코드를 입력해 주세요.")
+
+if code and date:
+    df = fdr.DataReader(code,date)
+    data =  df.sort_index(ascending=True).loc[:,'Close']
+    # st.line_chart(data)
+    tab1, tab2 = st.tabs(['차트','데이터'])
+
+    with tab1:
+        st.line_chart(data)
+    with tab2:
+        st.dataframe(df.sort_index(ascending=False))
+    with st.expander('컬럼 설명'):
+        st.markdown('''
+                    - Open : 시가
+                    - High : 고가
+                    - Low : 저가
+                    - Close : 종가
+                    - adj Close : 수정 종가
+                    - Volumn : 거래량
+                    ''')
